6b025d379888b0f558cc4f3f6707cf4e1dad6931
Joske Vermeulen <joske.vermeulen@ap.be>
2023-11-21 12:40:23 +0000

    Update data

--- blog/src/data.ts
+++ blog/src/data.ts
@@ -27,7 +27,7 @@ export const posts = [
         id: 1,
         title: 'Feline Physics: Unraveling the Mystery of Cat Leaps',
         excerpt: 'Understanding the astounding agility and physics behind cat jumps.',
-        image: '/cat.png',
+        image: '/blog-id-1.png',
         fullText: 'In this fascinating study, Purrtecch University’s physics department examines the biomechanics and physical principles behind the extraordinary jumping abilities of cats. Using high-speed cameras and motion analysis technology, our cat scientists reveal the secrets of feline agility and how it can inspire advancements in robotics and biomechanics.',
         date: new Date('2021-10-01')
     },
@@ -35,7 +35,7 @@ export const posts = [
         id: 2,
         title: 'The Purrfect Chemistry: Catnip and Feline Behavior',
         excerpt: 'Exploring the molecular magic behind catnip’s effect on cats.',
-        image: '/cat.png',
+        image: '/blog-id-2.png',
         fullText: 'Join our cat chemists as they delve into the compounds of catnip and their effects on feline behavior. This article from Purrtecch University’s chemistry department discusses the neurochemical reactions caused by catnip, providing a unique perspective on how certain substances can alter animal behavior.',
         date: new Date('2021-09-15')
     },
@@ -43,7 +43,7 @@ export const posts = [
         id: 3,
         title: 'Whisker Technology: Sensory Innovation Inspired by Cats',
         excerpt: 'How cat whiskers are inspiring the next generation of sensory tech.',
-        image: '/cat.png',
+        image: '/blog-id-3.png',
         fullText: 'Purrtecch University’s engineering department presents groundbreaking research on bio-inspired sensors modeled after cat whiskers. Learn about how the sensitivity and functionality of cat whiskers are being mimicked to develop advanced tactile sensors for robots, enhancing their ability to navigate and interact with their environment.',
         date: new Date('2021-08-20')
     },
@@ -51,7 +51,7 @@ export const posts = [
         id: 4,
         title: 'Meowlogies: Understanding Cat Communication',
         excerpt: 'Decoding the complex language of cats.',
-        image: '/cat.png',
+        image: '/blog-id-4.png',
         fullText: 'In this intriguing research, Purrtecch University’s linguistics and animal behavior experts investigate the various forms of cat communication. From vocalizations to body language, this comprehensive study provides insight into how cats convey messages and emotions, offering a deeper understanding of interspecies communication.',
         date: new Date('2021-07-10')
     },
@@ -59,7 +59,7 @@ export const posts = [
         id: 5,
         title: 'Catronomy: Feline Perspectives on the Cosmos',
         excerpt: 'Astronomical discoveries guided by the night vision of cats.',
-        image: '/cat.png',
+        image: '/blog-id-5.png',
         fullText: 'Explore the universe through the eyes of cats in this innovative research from Purrtecch University’s astronomy department. Utilizing the superior night vision of cats, our feline astronomers provide unique observations of celestial bodies and phenomena, shedding new light on the mysteries of space.',
         date: new Date('2021-06-01')
     }
